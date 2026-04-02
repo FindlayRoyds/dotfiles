8b5455623455402e5bcec4ceda2b6b14593f180f
Ryan Scofield <ryan.scofield@alliedtelesis.co.nz>
2026-04-02 16:01:46 +1300

    catppuccin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -82,15 +82,16 @@ require("lazy").setup({
         end,
     },
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = false,
         priority = 1000,
         opts = {
-            style = "night",
+            flavour = "mocha", -- options: latte, frappe, macchiato, mocha
         },
         config = function(_, opts)
-            require("tokyonight").setup(opts)
-            vim.cmd([[colorscheme tokyonight]])
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme "catppuccin"
         end,
     },
     {
