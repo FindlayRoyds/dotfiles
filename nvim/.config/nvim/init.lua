c945fa1ba95880b301fac24c449714740e386598
Findlay Royds <findlay.royds@alliedtelesis.co.nz>
2026-04-07 09:41:39 +1200

    add lua formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
-    -- GENERAL SETTINGS
+-- GENERAL SETTINGS
 vim.g.mapleader = " " -- Leader key for shortcuts
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus" -- Share yank register with system clipboard
@@ -14,39 +14,41 @@ vim.opt.splitright = true -- Open vertical splits to the right
 vim.opt.splitbelow = true -- Open horizontal splits below
 vim.opt.signcolumn = "yes" -- Show diagnostics in line numbers
 vim.diagnostic.config({
-  severity_sort = true, -- Prioritise showing E>W>H diagnostics
+	severity_sort = true, -- Prioritise showing E>W>H diagnostics
 })
 vim.opt.cursorline = true -- Highlight current line number
 vim.opt.cursorlineopt = "number" -- Don't highlight entire line only number
 vim.opt.shortmess:append("I") -- Hide slpash screen stuff
 vim.opt.wrap = false -- Stop lines wrapping
 
-    -- KEYBINDS
-vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>', { desc = 'Toggle terminal' })
+-- KEYBINDS
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
-vim.keymap.set('n', '<leader>h', '<C-w>h')
-vim.keymap.set('n', '<leader>j', '<C-w>j')
-vim.keymap.set('n', '<leader>k', '<C-w>k')
-vim.keymap.set('n', '<leader>l', '<C-w>l')
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
 
-vim.keymap.set('n', '<leader>H', '<C-w>H')
-vim.keymap.set('n', '<leader>J', '<C-w>J')
-vim.keymap.set('n', '<leader>K', '<C-w>K')
-vim.keymap.set('n', '<leader>L', '<C-w>L')
+vim.keymap.set("n", "<leader>H", "<C-w>H")
+vim.keymap.set("n", "<leader>J", "<C-w>J")
+vim.keymap.set("n", "<leader>K", "<C-w>K")
+vim.keymap.set("n", "<leader>L", "<C-w>L")
 
-vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "LSP Definition (Snacks)" })
+vim.keymap.set("n", "gd", function()
+	Snacks.picker.lsp_definitions()
+end, { desc = "LSP Definition (Snacks)" })
 
-vim.api.nvim_create_autocmd('TermOpen', {
-    group = vim.api.nvim_create_augroup('custom-terminal-config', { clear = true }),
-    callback = function()
-        vim.keymap.set('t', '<Esc><Esc>', [[<C-\><C-n>]], { buffer = 0, desc = 'Exit terminal mode' })
-        vim.keymap.set('t', '<Space>', '<Space>', { buffer = 0 }) -- Force Space to be sent immediately in Terminal mode
-    end,
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-terminal-config", { clear = true }),
+	callback = function()
+		vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { buffer = 0, desc = "Exit terminal mode" })
+		vim.keymap.set("t", "<Space>", "<Space>", { buffer = 0 }) -- Force Space to be sent immediately in Terminal mode
+	end,
 })
 
 vim.keymap.set("n", "<leader>s", function()
-  vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = true })
 end, { desc = "Format current buffer" })
 
 -- tabs
@@ -56,10 +58,10 @@ vim.opt.expandtab = true
 
 -- Don't continue comments onto newlines
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 
 -- Relative line numbers
@@ -70,162 +72,185 @@ vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true -- Uses case when typing capital letter
 
-    -- LAZY.NVIM PLUGINS
+-- LAZY.NVIM PLUGINS
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-       "m4xshen/hardtime.nvim",
-       lazy = false,
-       dependencies = { "MunifTanjim/nui.nvim" },
-       opts = {},
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "VeryLazy",
-    },
-    {
-        "erl-koenig/theme-hub.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-        require("theme-hub").setup({
-          auto_install_on_select = true,
-        })
-        end,
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            flavour = "mocha", -- options: latte, frappe, macchiato, mocha
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme "catppuccin"
-        end,
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({})
-        end
-    },
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        config = function()
-            require("toggleterm").setup({
-                open_mapping = nil,
-                direction = "float",
-                start_in_insert = true,
-                persist_size = true,
-                on_open = function(term) -- Put terminal in insert every time it's opened
-                    vim.schedule(function()
-                        vim.cmd("startinsert!")
-                    end)
-                end,
-            })
-        end
-    },
-    {
-        "okuuva/auto-save.nvim",
-        event = { "TextChanged" },
-        opts = {
-            enabled = true,
-            debounce_delay = 500,
-            condition = function(buf)
-                local fn = vim.fn
-                local utils = require("auto-save.utils.data")
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"erl-koenig/theme-hub.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("theme-hub").setup({
+				auto_install_on_select = true,
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			flavour = "mocha", -- options: latte, frappe, macchiato, mocha
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = nil,
+				direction = "float",
+				start_in_insert = true,
+				persist_size = true,
+				on_open = function(term) -- Put terminal in insert every time it's opened
+					vim.schedule(function()
+						vim.cmd("startinsert!")
+					end)
+				end,
+			})
+		end,
+	},
+	{
+		"okuuva/auto-save.nvim",
+		event = { "TextChanged" },
+		opts = {
+			enabled = true,
+			debounce_delay = 500,
+			condition = function(buf)
+				local fn = vim.fn
+				local utils = require("auto-save.utils.data")
 
-                -- Don't save for gitcommit, gitrebase, or non-modifiable files
-                if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), { "gitcommit", "gitrebase" }) then
-                    return true
-                end
-                return false
-            end,
-        },
-    },
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = true
-    },
-    {
-        "folke/snacks.nvim",
-        opts = {
-            picker = {
-                enabled = true,
-                formatters = {
-                    file = {
-                        filename_first = true,
-                    }
-                },
-                matcher = {
-                    cwd_bonus = true,
-                    frecency = true,
-                    history_bonus = true,
-                }
-            },
-        },
-        keys = {
-            { "<leader>f", function() Snacks.picker.smart({ filter = { cwd = true } }) end, desc = "Smart find (files + recent)" },
-            { "<leader>p", function() Snacks.picker() end, desc = "All pickers" },
-        },
-    },
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^8',
-        lazy = false,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "ruff", "ty" },
-            })
+				-- Don't save for gitcommit, gitrebase, or non-modifiable files
+				if
+					fn.getbufvar(buf, "&modifiable") == 1
+					and utils.not_in(fn.getbufvar(buf, "&filetype"), { "gitcommit", "gitrebase" })
+				then
+					return true
+				end
+				return false
+			end,
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				enabled = true,
+				formatters = {
+					file = {
+						filename_first = true,
+					},
+				},
+				matcher = {
+					cwd_bonus = true,
+					frecency = true,
+					history_bonus = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>f",
+				function()
+					Snacks.picker.smart({ filter = { cwd = true } })
+				end,
+				desc = "Smart find (files + recent)",
+			},
+			{
+				"<leader>p",
+				function()
+					Snacks.picker()
+				end,
+				desc = "All pickers",
+			},
+		},
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^8",
+		lazy = false,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "ruff", "ty", "stylua" },
+			})
 
-            vim.lsp.enable("ty") 
-            vim.lsp.enable("ruff") 
+			vim.lsp.enable("ty")
+			vim.lsp.enable("ruff")
+			vim.lsp.enable("stylua")
 
-            vim.lsp.config("ty", {
-                autostart = true,
-                capabilities = { offsetEncoding = { "utf-16" } }
-            })
-            vim.lsp.config("ruff", {
-                autostart = true,
-            })
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        opts = {
-            auto_install = true,
+			vim.lsp.config("ty", {
+				autostart = true,
+				capabilities = { offsetEncoding = { "utf-16" } },
+			})
+			vim.lsp.config("ruff", {
+				autostart = true,
+			})
+			vim.lsp.config("stylua", {
+				autostart = true,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			auto_install = true,
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 
-            indent = { enable = true },
-        },
-    },
+			indent = { enable = true },
+		},
+	},
 }, {
-    install = { colorscheme = { theme } },
+	install = { colorscheme = { theme } },
 })
