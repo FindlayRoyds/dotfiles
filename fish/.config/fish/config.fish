21bd8e9ff6ce7db5266afaae65afd1210027f10e
Findlay Royds <findlay.royds@alliedtelesis.co.nz>
2026-05-13 16:54:57 +1200

    Update fish config, set nvim as editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
+    set fish_greeting # no more welcome message
 
-    zoxide init fish | source
+    zoxide init fish | source # make zoxide work
+
+    # fish_vi_key_bindings # enable vi-mode
+    set -g fish_cursor_default block # block cursor in normal mode
+    set -g fish_cursor_insert line # line cursor in insert mode
+    function fish_mode_prompt; end # disable vi-mode prompt
+
+    set -gx EDITOR nvim # Open nvim as editor
+    set -gx VISUAL nvim # Open nvim as editor
 end
