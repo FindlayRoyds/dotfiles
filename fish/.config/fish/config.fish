da0dfc05a2674ac72cdb6f94a763870f287a7321
Findlay Royds <findlay.royds@alliedtelesis.co.nz>
2026-05-13 17:08:31 +1200

    Disable vim mode in fish shell properly this time

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,10 +3,15 @@ if status is-interactive
 
     zoxide init fish | source # make zoxide work
 
+    # vim mode
     # fish_vi_key_bindings # enable vi-mode
-    set -g fish_cursor_default block # block cursor in normal mode
-    set -g fish_cursor_insert line # line cursor in insert mode
-    function fish_mode_prompt; end # disable vi-mode prompt
+    # set -g fish_cursor_default block # block cursor in normal mode
+    # set -g fish_cursor_insert line # line cursor in insert mode
+    # function fish_mode_prompt; end # disable vi-mode prompt
+
+    # non vim mode
+    fish_default_key_bindings # disable vi-mode
+    set -g fish_cursor_default line # line cursor
 
     set -gx EDITOR nvim # Open nvim as editor
     set -gx VISUAL nvim # Open nvim as editor
