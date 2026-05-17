f00bb147d6af496c11b9228f2732fca568f68079
Findlay Royds <findlay.royds@alliedtelesis.co.nz>
2026-05-18 09:38:41 +1200

    Only evaluate homebrew if it exists

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
-
-eval (/opt/homebrew/bin/brew shellenv)
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
 
 if status is-interactive
     set fish_greeting # no more welcome message
